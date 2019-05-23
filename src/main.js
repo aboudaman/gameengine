3cc47e5c8ef77636355ebaa086bd781616b4b5d9
aboudaman <adaman2000@gmail.com>
2019-05-22 20:01:49 -0400

    second commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,55 @@
-const message = "We are making a game";
+import KeyControls from "../resources/lib/KeyConytrols"
 
-// alert($`We are making a {message}`);
-alert(`We are making a ${message}`);
+
+//Game setup code
+const canvas = document.querySelector("#game canvas");
+const ctx = canvas.getContext("2d");
+const { width: w, height: h } = canvas;
+let dt = 0;
+let last = 0;
+const speed = 64;
+let p1 = 0;
+let p2 = 0;
+const controls = new KeyControls();
+
+let x = w / 2;
+let y = h / 2;
+let color = 10;
+
+//ctx.globalAlpha = 0.02;
+
+function loop(ms) {
+    requestAnimationFrame(loop);
+
+    //set time in seconds
+    const t = ms/1000;
+
+    //Time elapsed since last frame
+    dt = t - last;
+    last = t;
+
+    //Game logic
+    ctx.save();
+
+    x += controls.x;
+    y += controls.y;
+
+    if (!controls.action) {
+        color += 10;
+        if (color > 360) {
+            color -= 360;
+        }
+    }
+
+    //Draw Rectangle
+    ctx.fillStyle = `hsl(${color}, 50%,50%)`;
+    ctx.fillRect(x,y,50,50);
+
+
+
+
+    ctx.restore();
+}
+
+//Game is running
+requestAnimationFrame(loop);
