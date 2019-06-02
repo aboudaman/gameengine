50e85528e135ab89ce88e9f6e6ec4d8c8ad7091e
aboudaman <adaman2000@gmail.com>
2019-06-02 13:33:39 -0400

    added background and plane

--- src/main.js
+++ src/main.js
@@ -7,18 +7,59 @@ const w = 640;
 const h = 480;
 const renderer = new CanvasRenderer(w,h);
 document.querySelector("#game").appendChild(renderer.view);
-
+const controls = new KeyControls();
 
 //Game Objects
 const scene = new Container();
-const planeObj = new Texture("../resources/images/fly.png");
 
-for (let i=0; i < 50; i++) {
-    const plane = new Sprite(planeObj);
-    plane.pos.x = Math.random() * w;
-    plane.pos.y = Math.random() * h;
-    scene.add(plane);
+// const planeTexture = new Texture("../resources/images/fly.png", {width:"10px", height:"10px"});
+
+// ## Setup Textures required for the game ##
+const textures = {
+    plane: new Texture("../resources/images/fly50.png"),
+    background: new Texture("../resources/images/BGcanvas.png")
 }
+
+
+// ## Set u the plane
+const plane = new Sprite(textures.plane);
+plane.pos.x = 20;
+plane.pos.y = h /2 - 180;
+
+plane.update = function(dt, t) {
+
+}
+
+
+//Load up the background
+
+const background = new Sprite(textures.background);
+
+// Add the background and the plane
+scene.add(background);
+scene.add(plane);
+
+
+
+
+// for (let i = 0; i < 5; i++) {
+//     const speed = Math.random() * 150 + 50;
+//     const plane = new Sprite(planeTexture);
+//     // plane.pos.x = Math.random() * w;
+//     // plane.pos.y = Math.random() * h;
+//     plane.pos.x = 20;
+//     plane.pos.y = 100;
+    
+//     // plane.update = function(dt) {
+//     //     this.pos.x += speed * dt;
+//     //     if (this.pos.x > w) {
+//     //         this.pos.x = -20;
+//     //     };
+//     // }
+
+//     scene.add(plane);
+    
+// }
 
 //Add Text using Text class
 const message = new Text("Game Engine", {
@@ -42,7 +83,7 @@ let last = 0;
 const speed = 64;
 let p1 = 0;
 let p2 = 0;
-const controls = new KeyControls();
+
 
 let x = w / 2;
 let y = h / 2;
@@ -59,7 +100,7 @@ function loop(ms) {
     //Time elapsed since last frame
     dt = t - last;
     last = t;
-   // scene.update(dt, t);
+    scene.update(dt, t);
     renderer.render(scene);
 }
 
